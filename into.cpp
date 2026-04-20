907ca957203b9fe67841703cc2f5f30bc0185d59
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-04-20 17:34:21 +0700

    menambahkan nim dan nama

---
+++ into.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+public:
+    int nim;
+    string nama;
+    float nilai;
+
+    void PrintData(){
+        cout << "NIM: " << nim << endl;
+        cout << "Nama: " << nama << endl;
+        cout << "Nilai: " << nilai << endl;
+    }
+};
+
+int main(){
+    Mahasiswa mhs;
+    mhs.nim = 2022;
+    mhs.nama = "abra";
+    mhs.nilai = 
+
+}
